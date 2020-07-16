04fd30f1312c6dc40b5fc5cbaa33108741f310cb
John L. Whiteman <johnlwhiteman@users.noreply.github.com>
2020-07-15 21:23:39 -0700

    Changed download url
    Signed-off-by: John L. Whiteman <johnlwhiteman@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     version='0.1.0',
     description='A simple filter for pandoc that runs shell commands and scripts and outputs as text and images.',
     url='https://github.com/johnlwhiteman/pandoc-run-filter',
-    download_url='https://github.com/johnlwhiteman/pandoc-run-filter/archive/v_001.tgz',
+    download_url='https://github.com/johnlwhiteman/pandoc-run-filter/archive/v0.1.0.tar.gz',
     author='John L. Whiteman',
     author_email='me@johnlwhiteman.com',
     license="BSD-3-Clause",
